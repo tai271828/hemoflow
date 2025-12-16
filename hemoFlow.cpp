d56e77fdef9d4160f8d9c0091844b12c68f243db
Taihsiang Ho (tai271828) <e@tai271828.me>
2025-12-16 14:20:37 +0100

    feat(xml-parser): parse new convergenceTolerance field

--- hemoFlow.cpp
+++ hemoFlow.cpp
@@ -48,6 +48,7 @@ string workingFolder;
 T simLength;
 T saveFreqTime;
 T checkpointFreqTime;
+T convergenceTolerance = 1e-11;
 
 // Vector of openings
 vector<OpeningHandler*> openings;
@@ -251,6 +252,15 @@ int main(int argc, char *argv[])
         catch (PlbIOException& exception) {
             pcout << "Warning: checkpointing tag was not found in config, checkpointing will be disabled!" << std::endl;
             useCheckpoint = false;
+        }
+
+        // Reading optional convergence tolerance
+        try {
+            xml["simulation"]["convergenceTolerance"].read(convergenceTolerance);
+        }
+        catch (PlbIOException& exception) {
+            pcout << "Warning: convergenceTolerance tag was not found in config. Using default value." << std::endl;
+            pcout << "Default convergence tolerance: " << convergenceTolerance << std::endl;
         }
 
         // Loading the input file
