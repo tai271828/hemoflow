8aa80a921c3ea9c27dea5196a6bf1d76e310b978
Tai Ho (tai271828) <e@tai271828.me>
2026-03-14 23:05:10 +0100

    enable debug mode etc.

--- preprocessor/main.py
+++ preprocessor/main.py
@@ -12,7 +12,7 @@ from detectOpenings import detectOpenings
 # Parameters to check before execution:
 SI_FACTOR = 0.001 # Ratio to [m]. Most STL is in [mm]
 
-DEBUG_MODE = False # This will enable additional intermediate nrrd output to check with e.g. 3DSlicer
+DEBUG_MODE = True # This will enable additional intermediate nrrd output to check with e.g. 3DSlicer
 #############################
 
 if DEBUG_MODE:
@@ -134,7 +134,7 @@ if __name__ == "__main__":
         nrrd.write(outputBaseName+"wall_fluid.nrrd", volWithWalls)
 
     print("Size after cutting layers for openings:", volWithWalls.shape)
-    volume = np.product(volWithWalls.shape)
+    volume = np.prod(volWithWalls.shape)
     fluids = np.count_nonzero(volWithWalls == 2)
     print("Volume:", volume)
     print("Fluid nodes:", fluids)
